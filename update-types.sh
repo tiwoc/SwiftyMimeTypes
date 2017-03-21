ded3a29b3a33c3a019a6157661a1e4e4a0e0a58c
Daniel Seither <post@tiwoc.de>
2017-03-21 17:21:31 +0100

    Add text/markdown type

--- update-types.sh
+++ update-types.sh
@@ -5,7 +5,14 @@ which shellcheck > /dev/null && shellcheck "$0"
 SCRIPTPATH="$(cd "$(dirname "$0")"; pwd)"
 
 APACHE_MIME_TYPES_URL="http://svn.apache.org/repos/asf/httpd/httpd/trunk/docs/conf/mime.types"
+ADDITIONAL_TYPES_PATH="$SCRIPTPATH/additional.types"
 FILEPATH="$SCRIPTPATH/SwiftyMimeTypes/Assets/mime.types"
 
-# sed removes lines starting with # and collapses consecutive whitespace chars to a single space
-curl "$APACHE_MIME_TYPES_URL" | sed -E '/#.*$/d;s/[[:space:]]+/ /g' > "$FILEPATH"
+function minify_types {
+    # removes lines starting with # and collapses consecutive whitespace chars to a single space
+    sed -E '/#.*$/d;s/[[:space:]]+/ /g'
+}
+
+curl "$APACHE_MIME_TYPES_URL" | minify_types > "$FILEPATH"
+< "$ADDITIONAL_TYPES_PATH"  minify_types >> "$FILEPATH"
+
